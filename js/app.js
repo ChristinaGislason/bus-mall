ba926762f91a07d40a037b0c3bab3d4436f50592
Christina Gislason <mkcgislason@gmail.com>
2018-08-13 17:10:49 -0700

    generate 3 new images, and event listeners

--- js/app.js
+++ js/app.js
@@ -8,40 +8,48 @@ function Item(filename) {
 }
 Item.allItems = [];
 
-var bag = new Item('img/bag.jpg');
-var banana = new Item('img/banana.jpg');
-var bathroom = new Item('img/bathroom.jpg');
-var boots = new Item('img/boots.jpg');
-var breakfast = new Item('img/breakfast.jpg');
-var bubblegum = new Item('img/bubblefum.jpg');
-var chair = new Item('img/chair.jpg');
-var cthulhu = new Item('img/cthulhu.jpg');
-var dogDuck = new Item('img/dog-duck.jpg');
-var dragon = new Item('img/dragon.jpg');
-var pen = new Item('img/pen.jpg');
-var petSweep = new Item('img/pet-sweep.jpg');
-var scissors = new Item('img/scissors.jpg');
-var shark = new Item('img/shark.jpg');
-var sweep = new Item('img/sweep.png');
-var tauntaun = new Item('img/tauntaun.jpg');
-var unicorn = new Item('img/unicorn.jpg');
-var usb = new Item('img/usb.gif');
-var waterCan = new Item('img/water-can.jpg');
-var wineGlass = new Item('img/wine-glass.jpg');
+new Item('img/bag.jpg');
+new Item('img/banana.jpg');
+new Item('img/bathroom.jpg');
+new Item('img/boots.jpg');
+new Item('img/breakfast.jpg');
+new Item('img/bubblefum.jpg');
+new Item('img/chair.jpg');
+new Item('img/cthulhu.jpg');
+new Item('img/dog-duck.jpg');
+new Item('img/dragon.jpg');
+new Item('img/pen.jpg');
+new Item('img/pet-sweep.jpg');
+new Item('img/scissors.jpg');
+new Item('img/shark.jpg');
+new Item('img/sweep.png');
+new Item('img/tauntaun.jpg');
+new Item('img/unicorn.jpg');
+new Item('img/usb.gif');
+new Item('img/water-can.jpg');
+new Item('img/wine-glass.jpg');
 
 
 function displayThreeNewItems() {
   // show 3 new pictures to user
   // grab 3 items at random
-  var randIndex = Math.floor(Math.random() * Item.allItems.length);
-  bag = Item.allItems[randIndex];
   do {
+    var randIndex = Math.floor(Math.random() * Item.allItems.length);
     var secondItemIndex = Math.floor(Math.random() * Item.allItems.length);
-  } while (randIndex === secondItemIndex);
-  item2 = Item.allItems[secondItemIndex];
-  // change img src on the page to match the 2 new items
+    var thirdItemIndex = Math.floor(Math.random() * Item.allItems.length);
+  } while (randIndex === secondItemIndex || randIndex === thirdItemIndex || secondItemIndex === thirdItemIndex);
+  var item1 = Item.allItems[randIndex];
+  var item2 = Item.allItems[secondItemIndex];
+  var item3 = Item.allItems[thirdItemIndex]; 
+  
+  
+  // change img src on the page to match the 3 new items
   img1.src = item1.filename;
+  item1.votes++;
   img2.src = item2.filename;
+  item2.vote++;
+  img3.src = item3.filename;
+  item3.vote++;
 }
 
 // event listeners
@@ -52,18 +60,20 @@ var img3 = document.getElementsByTagName('img')[2];
 // what are we listening for? click
 img1.addEventListener('click', function() {
   // add to votes for that item
-  item1.votes++;
+  // item1.votes++;
   // item1.votes = item1.votes + 1;
   displayThreeNewItems();
 });
 
 img2.addEventListener('click', function() {
-  // what should we do?
+  // what should we do? 
   // add to votes for that item
   item2.votes++;
   displayThreeNewItems();
+});
 
 img3.addEventListener('click', function() {
   item3.votes++;
   displayThreeNewItems();
+});
 
